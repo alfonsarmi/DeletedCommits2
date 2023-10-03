cc15f2b3b01f0836e5216757694e5f68ceb36563
Alfonso Sarmiento Ráez <asarmiento@foreworth.com>
2023-10-03 12:23:53 +0200

    Another Commit in other branch

---
+++ basic/Wrong.java
@@ -0,0 +1,34 @@
+/*
+ * JDK16
+ * 
+ * //It is valid.
+ * 
+ */
+public class Wrong {
+    final int myNum = 15;
+
+    /*
+     * This is a method.
+     * 
+     * Example of a method.
+     */
+    public static void main(String[] args) {/* Another one */
+        //Another type of comment.
+        System.out.println("Hello World");//This is a comment.
+
+        String name = "John";
+        System.out.println(name);
+        int myNum = 15;
+        System.out.println(myNum);
+        int myNum;
+        myNum = 15;
+        System.out.println(myNum);
+        
+        //Variables:
+        int myNum = 5;
+        float myFloatNum = 5.99f;
+        char myLetter = 'D'
+        boolean myBool = true;
+        String myText = "Hello";
+    }
+  
